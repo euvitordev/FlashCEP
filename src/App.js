b67a087a243b2da33fe989ac5d9cde699c23a80f
euvitordev <dev.vitorlucas@gmail.com>
2023-01-23 14:49:15 -0300

    finalizing the initial structure of the app.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
+import { useState } from 'react';
 import { FiSearch } from 'react-icons/fi';
 import './app.css';
 function App() {
+
+  const [input, setInput] = useState('')
+
+  function handleSearch(){
+    alert('teste')
+  }
+
   return (
     <div className="container">
-      <h1 className="title">Buscador de CEP</h1>
+      <h1 className="title">Endereço Rápido</h1>
 
       <div className="containerInput">
-        <input type="text" name="" id="" placeholder="Digite seu cep..."/>
-        <button className="buttonSearch">
+        <input type="text" 
+        name="" id="" 
+        placeholder="Digite seu cep..."
+        value={input}
+        onChange={(event) => setInput(event.target.value) }
+        />
+        <button className="buttonSearch" onClick={handleSearch}>
           <FiSearch size={25} color="#FFF"/>
         </button>
       </div>
